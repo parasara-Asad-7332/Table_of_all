805515883033b3a973469af026e2130607609583
parasara-Asad-7332 <kingofparasara786@gmail.com>
2025-08-16 08:00:43 +0530

    Add files via upload

---
+++ tables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int num;
+    printf("Enter the number you can table: ");
+    scanf("%d",&num);
+    for (int i = 1; i <= 10; i++)
+    {
+        int table = num * i;
+        
+        printf("%d * %d = %d\n",num,i,table);
+    }
+    
+    return 0;
+}
